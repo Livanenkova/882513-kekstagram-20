79c01e0f2574e3e9d5b06ef6045f75cc564f4325
Livanenkova <arina909@yandex.ru>
2020-07-24 12:23:10 +0300

    test

--- js/form.js
+++ js/form.js
@@ -4,13 +4,10 @@
   // Блок загрузки фотографий form.js
   var uploadFile = document.querySelector('#upload-file');
   var uploadCancel = document.querySelector('#upload-cancel');
-  var sliderPin = document.querySelector('.effect-level__pin');
-  var line = document.querySelector('.effect-level__line');
-  var effect = document.querySelector('.effect-level__value');
   var imgUploadPreview = document.querySelector('.img-upload__preview img');
   var imgUploadControl = document.querySelector('.img-upload__scale');
   var imgControlBig = imgUploadControl.querySelector('.scale__control--bigger');
-  var imgcontrolSmall = imgUploadControl.querySelector('.scale__control--smaller');
+  var imgControlSmall = imgUploadControl.querySelector('.scale__control--smaller');
   var imgControlValue = document.querySelector('.scale__control--value');
   var imgUploadOverlay = document.querySelector('.img-upload__overlay');
 
@@ -52,7 +49,7 @@
   var MAX_SCALE_VALUE = 100;
   var SCALE_STEP = 25;
 
-  imgcontrolSmall.addEventListener('click', function () {
+  imgControlSmall.addEventListener('click', function () {
     var scaleValue = Number(imgControlValue.value.slice(0, -1));
     scaleValue -= SCALE_STEP;
 
@@ -82,99 +79,8 @@
     imgUploadPreview.style.transform = 'scale(' + (value / 100) + ')';
   }
 
-  // Функция наложения эффекта на изображение form.js
-
-  function onFilterChange(event) {
-    imgUploadPreview.className = '';
-    imgUploadPreview.style.filter = null;
-    // imgUploadPreview.classList.add('effects__preview--' + event.target.value);
-
-    window.utils.addClass(imgUploadPreview, 'effects__preview--' + event.target.value);
-    if (event.target.value === 'none') {
-      window.utils.addClass(effectLevelSlider, 'hidden');
-    } else {
-      window.utils.removeClass(effectLevelSlider, 'hidden');
-    }
-  }
-
-  // Обработчик движения слайдера form.js
-
-  line.addEventListener('mousedown', function (evt) {
-
-    var offSet = sliderPin.offsetLeft;
-    var lineWidth = line.offsetWidth;
-    var effectValue = Math.round(offSet * 100 / lineWidth);
-    effect.value = effectValue;
-    var effectDepth = line.querySelector('.effect-level__depth');
-
-    var startCoords = {
-      x: evt.clientX
-    };
-
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      var shift = {
-        x: startCoords.x - moveEvt.clientX
-      };
-
-      startCoords = {
-        x: moveEvt.clientX
-      };
-
-      var newX = sliderPin.offsetLeft - shift.x;
-
-      if (newX >= 0 && newX <= lineWidth) {
-
-        // var offSet = sliderPin.offsetLeft;
-        // var effectValue = Math.round(offSet * 100 / lineWidth);
-        effect.value = effectValue;
-        sliderPin.style.left = (newX) + 'px';
-        effectDepth.style.width = (newX * 100 / lineWidth) + '%';
-      }
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
-
-
-  var imgEffectsContainer = document.querySelector('.img-upload__effects');
-  imgEffectsContainer.addEventListener('change', onFilterChange);
-
-  var effectLevelSlider = document.querySelector('.img-upload__effect-level');
-  var effectLevelInput = effectLevelSlider.querySelector('.effect-level__value');
-
-  function onEffectLevelChange() {
-
-    switch (imgUploadPreview.className) {
-      case 'effects__preview--chrome':
-        imgUploadPreview.style.filter = 'grayscale(' + (effectLevelInput.value / 100) + ')';
-        break;
-      case 'effects__preview--sepia':
-        imgUploadPreview.style.filter = 'sepia(' + (effectLevelInput.value / 100) + ')';
-        break;
-      case 'effects__preview--marvin':
-        imgUploadPreview.style.filter = 'invert(' + effectLevelInput.value + '%)';
-        break;
-      case 'effects__preview--phobos':
-        imgUploadPreview.style.filter = 'blur(' + (effectLevelInput.value * 3 / 100) + 'px)';
-        break;
-      case 'effects__preview--heat':
-        imgUploadPreview.style.filter = 'brightness(' + (effectLevelInput.value * 3 / 100 + 1) + ')';
-        break;
-      default:
-        imgUploadPreview.style.filter = null;
-    }
-  }
-
-  line.addEventListener('mousemove', onEffectLevelChange);
+  window.form = {
+    imgUploadPreview: imgUploadPreview
+  };
 
 })();
